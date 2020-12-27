8cbed1ccbf75a0c7361293fc08c343b42d39676f
diagnosa <d.fenomena@live.com>
2020-12-27 20:50:10 +0700

    add WIP display on TV

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import Container from '../components/container';
+import Display from '../components/display';
 import Television from '../components/television';
 
 export default function Home() {
     return (
         <Container>
             <h1>Hi! Let's join the adventure of Ketupat around Indonesia!</h1>
-            <p>
-                Change the channel to jump into another province
-            </p>
+            <p>Change the channel to jump into another province</p>
             <Television>
-                <span>ketupat adventure</span>
+                <Display />
             </Television>
         </Container>
     );
