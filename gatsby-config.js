b57d5165089db69f78e19e77257cd43f7ebc4364
diagnosa <d.fenomena@live.com>
2020-12-28 10:52:49 +0700

    add page manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,37 +1,50 @@
 const path = require('path');
 
 module.exports = {
-  siteMetadata: {
-    title: 'Ketupat Animated',
-    description: 'Ketupat aninmated using GatsbyJS',
-  },
-  plugins: [
-    {
-      resolve: 'gatsby-plugin-typography',
-      options: {
-        pathToConfigModule: 'src/utils/typography',
-      },
+    siteMetadata: {
+        title: 'Ketupat Animated',
+        description: 'Ketupat aninmated using GatsbyJS',
     },
-    {
-      resolve: 'gatsby-plugin-eslint',
-      options: {
-        test: /\.js$|\.jsx$/,
-        exclude: /(node_modules|.cache|public)/,
-        stages: ['develop'],
-        options: {
-          emitWarning: true,
-          failOnError: false,
+    plugins: [
+        {
+            resolve: 'gatsby-plugin-typography',
+            options: {
+                pathToConfigModule: 'src/utils/typography',
+            },
         },
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: path.join(__dirname, 'src', 'images'),
-      },
-    },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-  ],
+        {
+            resolve: 'gatsby-plugin-eslint',
+            options: {
+                test: /\.js$|\.jsx$/,
+                exclude: /(node_modules|.cache|public)/,
+                stages: ['develop'],
+                options: {
+                    emitWarning: true,
+                    failOnError: false,
+                },
+            },
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: path.join(__dirname, 'src', 'images'),
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Ketupat Animated`,
+                short_name: `Ketupad`,
+                start_url: `/`,
+                background_color: `#fefae0`,
+                theme_color: `#264653`,
+                display: `standalone`,
+                icon: `src/images/ketupat-icon.png`,
+                crossOrigin: `use-credentials`,
+            },
+        },
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-sharp',
+    ],
 };
